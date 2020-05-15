52f5000d571ffb85f057d47f83ce7bd1060fde1f
Unero <20809454+Unero@users.noreply.github.com>
2020-05-15 22:17:54 +0700

    Update gudang.sql

--- db/gudang.sql
+++ db/gudang.sql
@@ -1,185 +1,205 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2020 at 05:17 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
-Source Server         : MySQL
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : gudang
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2020-05-15 22:12:11
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `gudang`
+--
 
--- ----------------------------
--- Table structure for brands
--- ----------------------------
-DROP TABLE IF EXISTS `brands`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
 CREATE TABLE `brands` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `company` text DEFAULT NULL,
   `address` text DEFAULT NULL,
-  `phone` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `phone` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of brands
--- ----------------------------
-INSERT INTO `brands` VALUES ('4', 'ABC Inc.', 'A', 'jljl', '0000');
-INSERT INTO `brands` VALUES ('5', 'Supreme', 'S', 'kwm', '222');
+--
+-- Dumping data for table `brands`
+--
 
--- ----------------------------
--- Table structure for categories
--- ----------------------------
-DROP TABLE IF EXISTS `categories`;
+INSERT INTO `brands` (`id`, `name`, `company`, `address`, `phone`) VALUES
+(4, 'ABC Inc.', 'A', 'jljl', '0000'),
+(5, 'Supreme', 'S', 'kwm', '222');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of categories
--- ----------------------------
-INSERT INTO `categories` VALUES ('4', 'Food');
-INSERT INTO `categories` VALUES ('5', 'Home Tools');
+--
+-- Dumping data for table `categories`
+--
 
--- ----------------------------
--- Table structure for company
--- ----------------------------
-DROP TABLE IF EXISTS `company`;
+INSERT INTO `categories` (`id`, `name`) VALUES
+(4, 'Food'),
+(5, 'Home Tools');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
 CREATE TABLE `company` (
   `company_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `created_at` date DEFAULT NULL,
-  `country` varchar(255) NOT NULL,
-  PRIMARY KEY (`company_name`)
+  `country` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of company
--- ----------------------------
-INSERT INTO `company` VALUES ('Sinar Emas', 'Jalan Soekarno Hatta', '082245641111', '2020-05-10', 'Indonesia');
+--
+-- Dumping data for table `company`
+--
 
--- ----------------------------
--- Table structure for items
--- ----------------------------
-DROP TABLE IF EXISTS `items`;
+INSERT INTO `company` (`company_name`, `address`, `phone`, `created_at`, `country`) VALUES
+('Sinar Emas', 'Jalan Soekarno Hatta', '082245641111', '2020-05-10', 'Indonesia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
 CREATE TABLE `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` int(255) NOT NULL,
   `qty` int(50) NOT NULL,
   `description` text NOT NULL,
   `room_id` int(11) DEFAULT NULL,
   `brand_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `pr_brand` (`brand_id`),
-  KEY `pr_category` (`category_id`),
-  KEY `pr_room` (`room_id`),
-  CONSTRAINT `pr_brand` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
-  CONSTRAINT `pr_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION,
-  CONSTRAINT `pr_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of items
--- ----------------------------
-INSERT INTO `items` VALUES ('2', 'Sapu', '15000', '250', 'Sapu', '4', '5', '5');
+--
+-- Dumping data for table `items`
+--
 
--- ----------------------------
--- Table structure for roles
--- ----------------------------
-DROP TABLE IF EXISTS `roles`;
+INSERT INTO `items` (`id`, `name`, `price`, `qty`, `description`, `room_id`, `brand_id`, `category_id`) VALUES
+(2, 'Sapu', 15000, 250, 'Sapu', 4, 5, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL,
+  `role_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of roles
--- ----------------------------
-INSERT INTO `roles` VALUES ('1', 'Administrator');
-INSERT INTO `roles` VALUES ('2', 'Owner');
-INSERT INTO `roles` VALUES ('3', 'Staff');
+--
+-- Dumping data for table `roles`
+--
 
--- ----------------------------
--- Table structure for rooms
--- ----------------------------
-DROP TABLE IF EXISTS `rooms`;
+INSERT INTO `roles` (`id`, `role_name`) VALUES
+(1, 'Administrator'),
+(2, 'Owner'),
+(3, 'Staff');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
 CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `location` varchar(225) DEFAULT NULL,
-  `desc` varchar(225) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  `desc` varchar(225) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of rooms
--- ----------------------------
-INSERT INTO `rooms` VALUES ('1', '1-A', 'Tempat makanan');
-INSERT INTO `rooms` VALUES ('2', '1-B', 'Tempat peralatan rumahan');
-INSERT INTO `rooms` VALUES ('3', '1-C', 'Tempat makanan beku');
-INSERT INTO `rooms` VALUES ('4', '1-D', 'Tempat perkakas');
+--
+-- Dumping data for table `rooms`
+--
 
--- ----------------------------
--- Table structure for shipping
--- ----------------------------
-DROP TABLE IF EXISTS `shipping`;
+INSERT INTO `rooms` (`id`, `location`, `desc`) VALUES
+(1, '1-A', 'Tempat makanan'),
+(2, '1-B', 'Tempat peralatan rumahan'),
+(3, '1-C', 'Tempat makanan beku'),
+(4, '1-D', 'Tempat perkakas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
 CREATE TABLE `shipping` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `item_id` int(11) DEFAULT NULL,
   `qty` int(50) DEFAULT NULL,
   `type` varchar(50) DEFAULT '',
   `store_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `time` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `item` (`item_id`),
-  KEY `store` (`store_id`),
-  KEY `user_shipping` (`user_id`),
-  CONSTRAINT `item` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_shipping` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `time` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of shipping
--- ----------------------------
-INSERT INTO `shipping` VALUES ('1', '2', '100', 'Out', '3', '9', '2020-05-15 22:10:20');
+--
+-- Dumping data for table `shipping`
+--
 
--- ----------------------------
--- Table structure for stores
--- ----------------------------
-DROP TABLE IF EXISTS `stores`;
+INSERT INTO `shipping` (`id`, `item_id`, `qty`, `type`, `store_id`, `user_id`, `time`) VALUES
+(1, 2, 100, 'Out', 3, 9, '2020-05-15 15:10:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
 CREATE TABLE `stores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of stores
--- ----------------------------
-INSERT INTO `stores` VALUES ('1', 'Mc Donald', 'Jalan Kayu Tangan');
-INSERT INTO `stores` VALUES ('3', 'Toko Jaya Abadi', 'Jalan Bendu');
+--
+-- Dumping data for table `stores`
+--
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
+INSERT INTO `stores` (`id`, `name`, `address`) VALUES
+(1, 'Mc Donald', 'Jalan Kayu Tangan'),
+(3, 'Toko Jaya Abadi', 'Jalan Bendu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -187,16 +207,163 @@ CREATE TABLE `users` (
   `phone` varchar(255) NOT NULL,
   `gender` varchar(11) DEFAULT 'Rahasia',
   `address` varchar(255) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `role` (`role_id`),
-  CONSTRAINT `role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+  `role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', '$2y$10$ZrBk2zWOLhPAaOhncDBJv.pKAfhFYywahFQXY4NXDmhOcaRtLdAfS', 'admin', 'admin@admin.com', '12345678910', 'Male', 'jalan jalan', '1');
-INSERT INTO `users` VALUES ('9', 'Unero', 'roooo', 'Unero', 'ram', '082237', 'Male', null, '2');
-INSERT INTO `users` VALUES ('16', 'rin-ss', 'Ringgo', 'Rina', 'ringgo@gmail.com', '082239', 'Female', null, '3');
-INSERT INTO `users` VALUES ('17', 'uxdox', 'uxdox', 'Unero', 'ramadhanbhg@gmail.com', '082236180971', 'Male', null, '3');
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `phone`, `gender`, `address`, `role_id`) VALUES
+(1, 'admin', '$2y$10$ZrBk2zWOLhPAaOhncDBJv.pKAfhFYywahFQXY4NXDmhOcaRtLdAfS', 'admin', 'admin@admin.com', '12345678910', 'Male', 'jalan jalan', 1),
+(9, 'Unero', 'roooo', 'Unero', 'ram', '082237', 'Male', NULL, 2),
+(16, 'rin-ss', 'Ringgo', 'Rina', 'ringgo@gmail.com', '082239', 'Female', NULL, 3),
+(17, 'uxdox', 'uxdox', 'Unero', 'ramadhanbhg@gmail.com', '082236180971', 'Male', NULL, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_name`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pr_brand` (`brand_id`),
+  ADD KEY `pr_category` (`category_id`),
+  ADD KEY `pr_room` (`room_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item` (`item_id`),
+  ADD KEY `store` (`store_id`),
+  ADD KEY `user_shipping` (`user_id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `shipping`
+--
+ALTER TABLE `shipping`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `pr_brand` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `pr_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `pr_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+--
+-- Constraints for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD CONSTRAINT `item` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user_shipping` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
